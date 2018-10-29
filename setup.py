29c77860be48e6dafe3496e4d9d0963ce414e366
whitequark <whitequark@whitequark.org>
2018-10-29 06:42:32 +0000

    Bump version to 1.0.
    I don't have any major changes planned, and new AST nodes are just
    a minor bump.

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
 setuptools.setup(
     name="itanium_demangler",
-    version="0.0.1",
+    version="1.0",
     author="whitequark",
     author_email="whitequark@whitequark.org",
     description="Pure Python parser for mangled itanium symbols",
-    long_description=long_description,
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/whitequark/python-itanium_demangler",
     packages=setuptools.find_packages(),
